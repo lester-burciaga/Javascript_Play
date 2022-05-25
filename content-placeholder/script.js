ffcd708505830b967e4573208c555b30a793ef90
lesterbv06 <bloodshot_x@hotmail.com>
2022-05-25 16:07:12 -0500

    web ui projects

---
+++ content-placeholder/script.js
@@ -0,0 +1,24 @@
+const header = document.getElementById('header');
+const title = document.getElementById('title');
+const excerpt = document.getElementById('excerpt');
+const profile_img = document.getElementById('profile_img');
+const nameLabel = document.getElementById('name');
+const dateLabel = document.getElementById('date');
+
+const animated_bgs = document.querySelectorAll('.animated-bg');
+const animated_bgs_texts = document.querySelectorAll('.animated-bg-text');
+
+setTimeout(getData, 2200);
+
+function getData() {
+  header.innerHTML = '<img src="./assets/laptop.jpg" alt="Work station" />';
+  title.innerHTML = 'Lorem ipsum dolor asdas';
+  excerpt.innerHTML = 'Lorem ipsum dolor asdas asdaptio moll asdikuhu';
+  profile_img.innerHTML =
+    '<img src="./assets/myPhoto.jpg" alt="Profile photo" />';
+  nameLabel.innerHTML = 'Lester Burciaga Villa';
+  dateLabel.innerHTML = 'May 25, 2022';
+
+  animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'));
+  animated_bgs_texts.forEach((bg) => bg.classList.remove('animated-bg-text'));
+}
